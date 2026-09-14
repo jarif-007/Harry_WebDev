713fb909803bd5d1bb6d5dfbecc86e2499147c09
Mohammad Faiaj Jarif <u2203012@student.cuet.ac.bd>
2026-09-14 19:07:02 +0600

    some indentation fixed

--- Video 67/script.js
+++ Video 67/script.js
@@ -1,5 +1,5 @@
 console.log("Hello world")
 
-document.body.firstElementChild 
-document.body.firstElementChild.childNodes 
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
 document.body.firstElementChild.children
